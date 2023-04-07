ddbba5bbbab7bfc64fb69839f1c2d7ba25d026a2
Son won cheol <xzxz7003@naver.com>
2023-04-07 16:37:21 +0900

    Java_Basic_part02 : 실수 표현( 부동 소수점 오류)

--- java_01/src/ch05/IntVariableTest.java
+++ java_01/src/ch05/IntVariableTest.java
@@ -7,6 +7,6 @@ public class IntVariableTest {
 
         long iVal = 12345678900L; // int시 too large - > –2,147,483,648 ~ 2,147,483,647
         System.out.println("iVal = " + iVal);
-        
+
     }
 }
