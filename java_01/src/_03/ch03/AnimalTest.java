903020ae7ed5ebcc9cc3c72e4f4c8e75b5d4eaca
Son won cheol <xzxz7003@naver.com>
2023-04-21 12:23:17 +0900

    <java_basic> :  다운캐스팅( instance of 타입 검사 후 변환)

--- java_01/src/_03/ch03/AnimalTest.java
+++ java_01/src/_03/ch03/AnimalTest.java
@@ -13,16 +13,26 @@ class Human extends Animal{
     public void move(){
         System.out.println("사람이 두발로 걷습니다.");
     }
+
+    public void read(){
+        System.out.println("책읽는다~");
+    }
 }
 
 class Tiger extends Animal{
     public void move(){
         System.out.println("호랑이 네발로 걷습니다.");
     }
+    public void hunt(){
+        System.out.println("호랑이 사냥한다~");
+    }
 }
 class Eagle extends Animal{
     public void move(){
         System.out.println("독수리 하늘을 납니다.");
+    }
+    public void fly(){
+        System.out.println("난다~");
     }
 }
 public class AnimalTest {
@@ -44,10 +54,36 @@ public class AnimalTest {
         animalMove(hAnimal);
         animalMove(tAnimal);
         animalMove(eAnimal);
+
+
+        AnimalTest animalTest = new AnimalTest();
+        animalTest.testDownCasting(animalList);
     }
 
     public static void animalMove(Animal animal){
         animal.move();
+    }
+
+    public void testDownCasting(ArrayList<Animal> list){
+        for (int i = 0; i < list.size(); i++) {
+            Animal animal = list.get(i);
+            if(animal instanceof Human){
+                Human human = (Human) animal;
+                human.read();
+            }
+
+            if(animal instanceof Tiger){
+                Tiger tiger = (Tiger) animal;
+                tiger.hunt();
+            }
+
+
+            if(animal instanceof Eagle){
+                Eagle eagle = (Eagle) animal;
+                eagle.fly();
+            }
+
+        }
     }
 }
 
