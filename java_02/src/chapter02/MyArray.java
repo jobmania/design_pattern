1661b301f55898063aa98e66f6cf382269f5cd0e
Son won cheol <xzxz7003@naver.com>
2023-05-10 16:35:24 +0900

    <java_basic> : (update) 충돌 해결

--- java_02/src/chapter02/MyArray.java
+++ java_02/src/chapter02/MyArray.java
@@ -2,7 +2,7 @@ package chapter02;
 
 public class MyArray {
 
-<<<<<<< HEAD
+
     int[] intArr;   	//int array
     int count;  		//개수
 
@@ -121,24 +121,4 @@ public class MyArray {
     }
 }
 
-=======
-    int[] intArr;
-    int count;
 
-    public int ARRAY_CAPACITY;
-    public static final int ERROR_NUM = -99999999;
-
-
-    public MyArray(){
-        count = 0;
-        ARRAY_CAPACITY = 10;
-        intArr = new int[ARRAY_CAPACITY];
-    }
-
-    public MyArray(int size){
-        count = 0;
-        ARRAY_CAPACITY = size;
-        intArr = new int[ARRAY_CAPACITY];
-    }
-}
->>>>>>> master
