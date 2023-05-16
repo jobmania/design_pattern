502e7aba575982507dab1280b0110d1b584220f6
Son won cheol <xzxz7003@naver.com>
2023-05-16 16:42:19 +0900

    <java_basic> :  제너릭 활용 1차

---
+++ java_02/src/chapter03/GenericPrinterTest.java
@@ -0,0 +1,16 @@
+package chapter03;
+
+public class GenericPrinterTest {
+
+    public static void main(String[] args) {
+        GenericPrinter<Powder> powderPrinter = new GenericPrinter<>();
+        powderPrinter.setMaterial(new Powder());
+        System.out.println(powderPrinter);
+
+        var skd = "엄";
+
+        GenericPrinter<Plastic> plasticPrinter = new GenericPrinter<>();
+        plasticPrinter.setMaterial(new Plastic());
+        System.out.println(plasticPrinter);
+    }
+}
---
+++ java_02/src/chapter03/Water.java
@@ -0,0 +1,8 @@
+package chapter03;
+
+public class Water {
+    @Override
+    public String toString() {
+        return "물입니다~~";
+    }
+}
